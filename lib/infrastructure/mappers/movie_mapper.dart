de6fabfc4838977911c552ffa5d9cc1a075468ea
Nacho <i.licona@outlook.com>
2023-11-25 16:24:37 -0600

    mapeo de clase

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -4,14 +4,18 @@ import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 class MovieMapper{
   static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
     adult: movieDB.adult,
-    backdropPath: movieDB.backdropPath,
+    backdropPath: (movieDB.backdropPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ movieDB.backdropPath }'
+    : 'https://linnea.com.ar/wp-content/uploads/2018/09/404PosterNotFound.jpg' ,
     genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
     id: movieDB.id,
     originalLanguage: movieDB.originalLanguage,
     originalTitle: movieDB.originalLanguage,
     overview: movieDB.overview,
     popularity: movieDB.popularity,
-    posterPath: movieDB.posterPath,
+    posterPath: (movieDB.posterPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ movieDB.posterPath }'
+    : 'https://linnea.com.ar/wp-content/uploads/2018/09/404PosterNotFound.jpg' ,
     releaseDate: movieDB.releaseDate,
     title: movieDB.title,
     video: movieDB.video,

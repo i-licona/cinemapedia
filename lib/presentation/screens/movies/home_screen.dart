e4f7f7bed3a4d939b152ba0277cd91667e8f6f44
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-12-02 21:42:55 -0600

    se finaliza la construccion de la arquitectura de la app

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -7,7 +10,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState(){
+    super.initState();
+    // realiza una peticion al API
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Esta al pendiente del provider
+    final List<Movie> nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    if (nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final Movie movie = nowPlayingMovies[index];
+        return ListTile(title: Text(movie.title),);
+      },
     );
   }
 }

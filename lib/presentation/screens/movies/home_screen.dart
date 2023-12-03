2368df6da09bff0e59586598431fd5dd8b8c86f7
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-12-02 22:13:47 -0600

    Custom appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -39,12 +40,19 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if (nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final Movie movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title),);
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final Movie movie = nowPlayingMovies[index];
+              return ListTile(title: Text(movie.title),);
+            },
+          ),
+        )
+      ],
     );
   }
 }
